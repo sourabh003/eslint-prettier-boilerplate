4c33923f82dd6d1f55b174193731005dcf27fdaa
sourabh <sourabh@techindustan.com>
2022-09-29 13:08:26 +0530

    test commit

--- pages/index.js
+++ pages/index.js
@@ -49,6 +49,14 @@ export default function Home() {
 					</a>
 
 					<a
+						href="https://github.com/vercel/next.js/tree/canary/examples"
+						className={styles.card}
+					>
+						<h2>Examples &rarr;</h2>
+						<p>Discover and deploy boilerplate example Next.js projects.</p>
+					</a>
+
+					<a
 						href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
 						className={styles.card}
 					>
